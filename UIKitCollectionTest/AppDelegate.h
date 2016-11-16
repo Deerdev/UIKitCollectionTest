cf0617d97141a27ce8ff0133753536c8a340e9fd
daoquan.lu <daoquan.lu@feixun.com.cn>
2016-11-16 09:28:23 +0800

    Initial Commit

---
+++ UIKitCollectionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKitCollectionTest
+//
+//  Created by daoquan on 2016/11/16.
+//  Copyright © 2016年 daoquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
